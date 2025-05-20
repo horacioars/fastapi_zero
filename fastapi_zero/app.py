bbac230b7fdf07e7b57d4b3c57c618601b85e2c4
Horacio Augusto Ribeiro de Siqueira <horacioars@outlook.com>
2025-05-19 21:45:55 -0300

    Adcionando Excercicio 02

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,9 @@
 # fastapi_zero/app.py
 from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 from fastapi_zero.schemas import Message
 
 app = FastAPI()
@@ -9,3 +12,16 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+        <head>
+            <title>Olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo, em formato HTML!</h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
@@ -7,5 +8,14 @@ from fastapi_zero.app import app
 def test_read_root():
     client = TestClient(app)
     response = client.get('/')
+
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, Mundo!'}
+
+
+def test_exercicio_aula_02():
+    client = TestClient(app)
+    response = client.get('/exercicio-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo, em formato HTML!</h1>' in response.text
